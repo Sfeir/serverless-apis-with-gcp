ab89383b2debd32edada1306a0330f1e876db900
Seifeddin MANSRI <mansri.s@sfeir.com>
2020-02-03 03:09:35 +0100

    Add App Engine Code

--- appengine/main.py
+++ appengine/main.py
@@ -1,28 +1,46 @@
-# Copyright 2016 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# [START gae_python37_app]
+from flask import Flask
+from flask import request
 
-import webapp2
+import logging
 
-DEFAULT_NAME = 'World'
+from google.cloud import datastore
 
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-	name = self.request.get('name', DEFAULT_NAME)
-        self.response.headers['Content-Type'] = 'text/html'
-        self.response.write('<h1>Appengine Standard</h1><h2>Hello, ' + name + '!</h2>')
+client = datastore.Client()
+
+ANCESTOR_KIND = 'Environment';
+KIND = 'Microservice';
+
+# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
+# called `app` in `main.py`.
+app = Flask(__name__)
 
 
-app = webapp2.WSGIApplication([
-    ('/', MainPage),
-], debug=True)
+@app.route('/', methods=["GET"])
+def appInventory():
+    year = request.args.get('year')
+
+    ancestor = client.key(ANCESTOR_KIND, 'App Engine')
+    query = client.query(kind=KIND, ancestor=ancestor)
+
+    if year != None :
+      try:
+        query.add_filter('year', '=', int(year)) 
+      except ValueError as ex:
+        print('"%s" cannot be converted to an int' % year)
+
+    result = '<h1>App Engine Applications Inventory</h1><ul>';
+    query_iter = query.fetch()
+    for microservice in query_iter:
+      result += '<li>' + microservice['name'] + ' (' + str(microservice['year']) + ')</li>'
+    result += '</ul>'
+
+    return result
+
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
+# [END gae_python37_app]
